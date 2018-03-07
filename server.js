51a5e72212caedaa3398bc7307993d33cf4f11d9
Kevin Farrow <kevnaf@gmail.com>
2018-03-06 16:18:11 -0800

    post update

--- server.js
+++ server.js
@@ -20,5 +20,26 @@ app.use(cors());
 // API Endpoints
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+app.post('/mons', bodyParser, (request, response) => {
+  client.query(`INSERT INTO pokemon(user_id, mon_nick, mon_name, image_url, wins, losses, levels, hp_stat, atk_stat, def_stat, satk_stat, sdef_stat, speed_stat) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`,
+    [
+      request.body.user_id,
+      request.body.mon_nick,
+      request.body.mon_name,
+      request.body.image_url,
+      request.body.wins,
+      request.body.losses,
+      request.body.levels,
+      request.body.hp_stat,
+      request.body.atk_stat,
+      request.body.def_stat,
+      request.body.satk_stat,
+      request.body.sdef_stat,
+      request.body.speed_stat,
+    ])
+    .then(() => response.send('Update Complete'))
+    .catch(console.error);
+});
+
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
